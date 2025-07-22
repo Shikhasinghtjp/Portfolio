61d0cf5f6189f592a01f0cdfc83b4a2f76bc16c8
shikhatjp@gmail.com <shikhatjp@gmail.com>
2025-07-22 12:07:32 +0530

    Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -139,8 +139,8 @@ const Navbar = () => {
     <Nav>
       <NavbarContainer>
         <NavLogo to="/">
-          <ColorText>&lt;</ColorText>Rishav
-          <div style={{ color: theme.primary }}>/</div>Chanda
+          <ColorText>&lt;</ColorText>Shikha Singh
+          <div style={{ color: theme.primary }}>/</div>Thakur
           <ColorText>&gt;</ColorText>
         </NavLogo>
 
