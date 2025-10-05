025f624e09ad9f98e3bc9ef7e1ff8d799ce0c26b
shikhatjp@gmail.com <shikhatjp@gmail.com>
2025-10-05 23:41:17 +0530

    Updated

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -256,7 +256,7 @@ const Hero = () => {
             <HeroRightContainer>
               <motion.div {...headContentAnimation}>
                 <Tilt>
-                  <Img src={HeroImg} alt="Rishav Chanda" />
+                  <Img src={HeroImg} alt="Shikha Singh Thakur" />
                 </Tilt>
               </motion.div>
             </HeroRightContainer>
